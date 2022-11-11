4c554382a64306f9552aa700b7f552ed040676b0
wymin576 <51251045+wymin576@users.noreply.github.com>
2022-11-12 06:15:26 +0800

    Update boosting.R

--- boosting.R
+++ boosting.R
@@ -42,3 +42,37 @@ w = w3$w*exp(-alpha*y*g)/sum(w3$w*exp(-alpha*y*g))
 f3 <-  w2$alpha*w2$g + w3$alpha*w3$g +alpha*g 
 
 sign(f3)==y
+
+
+# boosting tree
+x <- 1:10;y <- c(5.56,5.70,5.91,6.40,6.80,7.05,8.90,8.70,9.00,9.05)
+
+z <- NULL;i=1
+while (i<10) {
+  z[i] <- (x[i]+x[i+1])/2
+  i=i+1
+}
+z
+
+myfunc <- function(y){
+m <- NULL;c1 <- NULL;c2 <- NULL;
+for (i in z){
+  
+  c1[i] <- mean(y[1:i])
+  c2[i]<- mean(y[(i+1):(length(x)+1)])
+  m[i] <- sum((y[1:i]-c1[i])^2)+sum((y[(i+1):(length(x)+1)]-c2[i])^2)
+}
+s=which.min(m);s
+c1 <-mean(y[1:z[s]]) ;c2 <- mean(y[(z[s]+1):(length(x)+1)])
+c1;c2  
+t1 <- ifelse(x < z[s],c1,c2)
+r2=y-t1;r2;L=sum(r2^2)
+result=list(z=z[s],c1=c1,c2=c2,r2=r2,L=L)
+return(result)
+}
+f1 <- myfunc(y);f1
+f2 <- myfunc(f1$r2);f2
+f3 <- myfunc(f2$r2);f3
+f4 <- myfunc(f3$r2);f4
+f5 <- myfunc(f4$r2);f5
+f6 <- myfunc(f5$r2);f6
